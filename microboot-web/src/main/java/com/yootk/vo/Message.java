92006efb8de09f2027988657a3770af804451848
李兴华 <784420216@qq.com>
2021-03-11 20:03:58 +0800

    0402_【掌握】整合FastJSON组件

--- microboot-web/src/main/java/com/yootk/vo/Message.java
+++ microboot-web/src/main/java/com/yootk/vo/Message.java
@@ -1,11 +1,15 @@
 package com.yootk.vo;
 
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
 @Data // 只要使用Lombok组件，99%的情况下就使用这一个注解
 public class Message {
     private String title; // 字符串的参数
+    @JsonFormat(pattern = "yyyy年MM月dd日") // FastJSON组件所提供的转换格式
+//    @JSONField(format = "yyyy年MM月dd日") // FastJSON组件所提供的转换格式
     private Date pubdate; // 日期参数
     private String content;
 }
