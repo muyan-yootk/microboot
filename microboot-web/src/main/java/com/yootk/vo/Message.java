9dec115793ce9dfea16aaf84f1127f7318e69745
李兴华 <784420216@qq.com>
2021-03-14 09:20:47 +0800

    0404_【理解】返回PDF数据

--- microboot-web/src/main/java/com/yootk/vo/Message.java
+++ microboot-web/src/main/java/com/yootk/vo/Message.java
@@ -2,16 +2,10 @@ package com.yootk.vo;
 
 import lombok.Data;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 @Data // 只要使用Lombok组件，99%的情况下就使用这一个注解
-@XmlRootElement // 配置XML根元素
 public class Message {
-    @XmlElement
     private String title; // 字符串的参数
-    @XmlElement
     private Date pubdate; // 日期参数
-    @XmlElement
     private String content;
 }
