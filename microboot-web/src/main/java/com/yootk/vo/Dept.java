9d6a870b7790335e4dc170dbb61f0b4834583371
李兴华 <784420216@qq.com>
2021-03-16 19:44:18 +0800

    0412_【掌握】自定义注入配置文件

--- microboot-web/src/main/java/com/yootk/vo/Dept.java
+++ microboot-web/src/main/java/com/yootk/vo/Dept.java
@@ -2,11 +2,13 @@ package com.yootk.vo;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
+@PropertySource(value = "classpath:muyan.properties", encoding = "UTF-8")
 @ConfigurationProperties(prefix = "object")
 @Component
 public class Dept {
