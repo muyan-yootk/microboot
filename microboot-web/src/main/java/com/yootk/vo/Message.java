1015cff9e3e54913be8f4678101253b08b498c74
李兴华 <784420216@qq.com>
2021-03-29 09:15:55 +0800

    0523_【理解】设置错误信息

--- microboot-web/src/main/java/com/yootk/vo/Message.java
+++ microboot-web/src/main/java/com/yootk/vo/Message.java
@@ -9,15 +9,15 @@ import javax.validation.constraints.NotNull;
 import java.util.Date;
 @Data // 只要使用Lombok组件，99%的情况下就使用这一个注解
 public class Message {
-    @NotBlank // 该字段的内容不允许为空
+    @NotBlank(message = "{message.title.notblank.error}") // 该字段的内容不允许为空
     private String title; // 字符串的参数
-    @NotNull // 该字段的内容不允许为空
+    @NotNull(message = "{message.pubdate.notnull.error}") // 该字段的内容不允许为空
     private Date pubdate; // 日期参数
-    @NotBlank // 该字段的内容不允许为空
+    @NotBlank(message = "{message.content.notblank.error}") // 该字段的内容不允许为空
     private String content;
-    @Email // 邮箱检测
-    @NotBlank
+    @Email(message = "{message.email.email.error}") // 邮箱检测
+    @NotBlank(message = "{message.email.notblank.error}")
     private String email;
-    @Digits(integer = 1, fraction = 0) // 1位整数，0位小数
+    @Digits(integer = 1, fraction = 0, message = "{message.level.digits.error}") // 1位整数，0位小数
     private Integer level; // 消息级别
 }
