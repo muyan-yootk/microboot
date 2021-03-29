12cd483a06493cd15ef0468b69e840509d407153
李兴华 <784420216@qq.com>
2021-03-29 08:55:58 +0800

    0522_【掌握】JSR303数据验证规范

--- microboot-web/src/main/java/com/yootk/vo/Message.java
+++ microboot-web/src/main/java/com/yootk/vo/Message.java
@@ -2,10 +2,22 @@ package com.yootk.vo;
 
 import lombok.Data;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 @Data // 只要使用Lombok组件，99%的情况下就使用这一个注解
 public class Message {
+    @NotBlank // 该字段的内容不允许为空
     private String title; // 字符串的参数
+    @NotNull // 该字段的内容不允许为空
     private Date pubdate; // 日期参数
+    @NotBlank // 该字段的内容不允许为空
     private String content;
+    @Email // 邮箱检测
+    @NotBlank
+    private String email;
+    @Digits(integer = 1, fraction = 0) // 1位整数，0位小数
+    private Integer level; // 消息级别
 }
