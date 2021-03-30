d5211853daf3cf4154d9f0a21ec890b4d9794a9f
李兴华 <784420216@qq.com>
2021-03-30 09:27:18 +0800

    0606_【理解】读取资源文件

--- microboot-thymeleaf/src/main/java/com/yootk/action/ThymeleafAction.java
+++ microboot-thymeleaf/src/main/java/com/yootk/action/ThymeleafAction.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller // 此时不是进行了Rest响应
 @RequestMapping("/thymeleaf/*") // 设置父路径
 public class ThymeleafAction {
-    @RequestMapping("path")
+    @RequestMapping("i18n")
     public String path() {
-        return "message/message_path"; // 跳转路径
+        return "message/message_i18n"; // 跳转路径
     }
 }
