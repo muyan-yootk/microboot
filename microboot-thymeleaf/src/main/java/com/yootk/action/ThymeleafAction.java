58b0b6493c1241b07abd361b18089b73edb2e1c1
李兴华 <784420216@qq.com>
2021-03-31 09:21:50 +0800

    0611_【理解】页面包含指令

--- microboot-thymeleaf/src/main/java/com/yootk/action/ThymeleafAction.java
+++ microboot-thymeleaf/src/main/java/com/yootk/action/ThymeleafAction.java
@@ -1,20 +1,13 @@
 package com.yootk.action;
 
-import com.yootk.vo.Member;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.text.SimpleDateFormat;
 
 @Controller // 此时不是进行了Rest响应
 @RequestMapping("/thymeleaf/*") // 设置父路径
 public class ThymeleafAction {
-    @RequestMapping("show")
-    public String show(Model model) throws Exception {
-        Member member = new Member("hello", "李兴华", 19, 888.66,
-                new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("1997-07-15 21:10:32"));
-        model.addAttribute("member", member);
-        return "member/member_show"; // 跳转路径
+    @RequestMapping("include")
+    public String include() throws Exception {
+        return "message/message_include"; // 跳转路径
     }
 }
