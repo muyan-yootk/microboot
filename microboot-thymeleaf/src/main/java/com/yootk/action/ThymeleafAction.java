905bca124d0e17924e387ea4495e2388e58df78f
李兴华 <784420216@qq.com>
2021-03-31 09:37:52 +0800

    0612_【理解】页面数据处理

--- microboot-thymeleaf/src/main/java/com/yootk/action/ThymeleafAction.java
+++ microboot-thymeleaf/src/main/java/com/yootk/action/ThymeleafAction.java
@@ -1,13 +1,20 @@
 package com.yootk.action;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+import java.util.Set;
 
 @Controller // 此时不是进行了Rest响应
 @RequestMapping("/thymeleaf/*") // 设置父路径
 public class ThymeleafAction {
-    @RequestMapping("include")
-    public String include() throws Exception {
-        return "message/message_include"; // 跳转路径
+    @RequestMapping("handle")
+    public String handle(Model model) throws Exception {
+        model.addAttribute("message", "www.YOOTK.com");
+        model.addAttribute("language", Set.of("Java", "Python", "GoLang"));
+        model.addAttribute("infos", Map.of("yootk", "yootk.com", "edu", "edu.yootk.com"));
+        return "message/message_handle"; // 跳转路径
     }
 }
