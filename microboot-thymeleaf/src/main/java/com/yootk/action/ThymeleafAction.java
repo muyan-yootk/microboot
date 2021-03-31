6ee95773494d6d49862c5179df9c342c8de235b4
李兴华 <784420216@qq.com>
2021-03-31 08:43:09 +0800

    0609_【理解】页面逻辑处理

--- microboot-thymeleaf/src/main/java/com/yootk/action/ThymeleafAction.java
+++ microboot-thymeleaf/src/main/java/com/yootk/action/ThymeleafAction.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
 
 @Controller // 此时不是进行了Rest响应
@@ -14,7 +12,7 @@ import java.text.SimpleDateFormat;
 public class ThymeleafAction {
     @RequestMapping("show")
     public String show(Model model) throws Exception {
-        Member member = new Member("yootk", "李兴华", 16, 888.66,
+        Member member = new Member("hello", "李兴华", 19, 888.66,
                 new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("1997-07-15 21:10:32"));
         model.addAttribute("member", member);
         return "member/member_show"; // 跳转路径
