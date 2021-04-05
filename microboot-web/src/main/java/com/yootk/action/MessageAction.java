cf8424cf2b84e0f61de86e49f8dc41489295a136
李兴华 <784420216@qq.com>
2021-04-05 08:39:58 +0800

    0708_【掌握】Lombok日志注解

--- microboot-web/src/main/java/com/yootk/action/MessageAction.java
+++ microboot-web/src/main/java/com/yootk/action/MessageAction.java
@@ -2,24 +2,24 @@ package com.yootk.action;
 
 import com.yootk.common.action.abs.AbstractBaseAction;
 import com.yootk.vo.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger; // 使用的是SLF4J标准
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 @RestController // 直接基于Rest架构进行处理，省略了@ResponseBody注解
 @RequestMapping("/message/*") // 添加父路径
 @Validated // 启用当前的JSR303注解
+@Slf4j // 直接进行日志的启用
+// 当使用了以上的注解之后就会自动的在当前的程序类里面出现有一个log的日志对象
 public class MessageAction extends AbstractBaseAction { // 控制层的实现类
     @RequestMapping("echo") // 子路径
     public Object echo(Message message) { // 进行请求参数的接收以及请求内容的回应
+        log.info("接收到了请求的参数：message = {}", message); // SLF4J的时候可以直接使用占位符
         message.setTitle("【ECHO】" + message.getTitle());
         message.setContent("【ECHO】" + message.getContent());
         return message;
-    }
-    @RequestMapping("get")
-    public Object get(String id) {  // 根据id加载数据信息
-        return "【YOOTK】" + id;
     }
 }
 // http://localhost/message/echo?title=沐言科技&content=www.yootk.com&pubdate=1998-09-19
