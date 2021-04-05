9bd2d65060b6800d998b53d1de512f2fdf85785f
李兴华 <784420216@qq.com>
2021-04-05 10:03:02 +0800

    0712_【掌握】MDC全链路跟踪

--- microboot-web/src/main/java/com/yootk/action/MessageAction.java
+++ microboot-web/src/main/java/com/yootk/action/MessageAction.java
@@ -1,28 +1,21 @@
 package com.yootk.action;
 
 import com.yootk.common.action.abs.AbstractBaseAction;
-import com.yootk.vo.Message;
+import com.yootk.service.IMessageService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController // 直接基于Rest架构进行处理，省略了@ResponseBody注解
 @RequestMapping("/message/*") // 添加父路径
-@Validated // 启用当前的JSR303注解
 @Slf4j // 直接进行日志的启用
-// 当使用了以上的注解之后就会自动的在当前的程序类里面出现有一个log的日志对象
 public class MessageAction extends AbstractBaseAction { // 控制层的实现类
+    @Autowired
+    private IMessageService message;
     @RequestMapping("echo") // 子路径
-    public Object echo(Message message) { // 进行请求参数的接收以及请求内容的回应
-        log.error("接收到了请求的参数：message = {}", message);
-        log.warn("接收到了请求的参数：message = {}", message);
-        log.info("接收到了请求的参数：message = {}", message);
-        log.debug("接收到了请求的参数：message = {}", message);
-        log.trace("接收到了请求的参数：message = {}", message);
-        message.setTitle("【ECHO】" + message.getTitle());
-        message.setContent("【ECHO】" + message.getContent());
-        return message;
+    public Object echo(String msg) { // 进行请求参数的接收以及请求内容的回应
+        log.info("控制层接收到用户请求，请求参数为：msg = {}", msg); // 日志输出
+        return this.message.echo(msg);
     }
 }
-// http://localhost/message/echo?title=沐言科技&content=www.yootk.com&pubdate=1998-09-19
-// https://localhost/message/echo?title=沐言科技&content=www.yootk.com&pubdate=1998-09-19
--- microboot-web/src/main/java/com/yootk/service/impl/MessageServiceImpl.java
+++ microboot-web/src/main/java/com/yootk/service/impl/MessageServiceImpl.java
@@ -1,12 +1,15 @@
 package com.yootk.service.impl;
 
 import com.yootk.service.IMessageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class MessageServiceImpl implements IMessageService {
     @Override
     public String echo(String msg) {
+        log.info("消息回应业务处理，传递的消息数据为：msg = {}", msg);
         return "【ECHO】" + msg;
     }
 }
