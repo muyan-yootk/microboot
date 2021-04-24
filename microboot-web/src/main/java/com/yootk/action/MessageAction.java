ede5df3aefc26e881be8a5a93e00ee8cce3cfbff
李兴华 <784420216@qq.com>
2021-04-24 09:56:34 +0800

    0902_【掌握】Callable实现异步处理

--- microboot-web/src/main/java/com/yootk/action/MessageAction.java
+++ microboot-web/src/main/java/com/yootk/action/MessageAction.java
@@ -1,21 +1,26 @@
 package com.yootk.action;
 
 import com.yootk.common.action.abs.AbstractBaseAction;
-import com.yootk.service.IMessageService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+
 @RestController // 直接基于Rest架构进行处理，省略了@ResponseBody注解
-@RequestMapping("/message/*") // 添加父路径
+@RequestMapping("/async/*") // 添加父路径
 @Slf4j // 直接进行日志的启用
 public class MessageAction extends AbstractBaseAction { // 控制层的实现类
-    @Autowired
-    private IMessageService message;
-    @RequestMapping("echo") // 子路径
+    @RequestMapping("callable") // 子路径
     public Object echo(String msg) { // 进行请求参数的接收以及请求内容的回应
-        log.info("控制层接收到用户请求，请求参数为：msg = {}", msg); // 日志输出
-        return this.message.echo(msg);
+        log.info("外部线程：{}", Thread.currentThread().getName()); // 日志输出
+        // 所有的子线程都是由主线程启动的，所以此时要观察主从两个线程的名称
+        return new Callable<String>() { // 返回一个异步线程
+            @Override
+            public String call() throws Exception {
+                log.info("内部线程：{}", Thread.currentThread().getName()); // 日志输出
+                return "【ECHO】" + msg; // 数据响应
+            }
+        };
     }
 }
