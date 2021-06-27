575a0de19750d3e3c51e6fa013f5fbc228737174
李兴华 <784420216@qq.com>
2021-06-27 09:38:21 +0800

    1216_【掌握】JWT结构分析

---
+++ microboot-jwt/src/main/java/com/yootk/StartJWTApplication.java
@@ -0,0 +1,11 @@
+package com.yootk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartJWTApplication {
+    public static void main(String[] args) { // 沐言科技：www.yootk.com
+        SpringApplication.run(StartJWTApplication.class, args); // 程序启动
+    }
+}
